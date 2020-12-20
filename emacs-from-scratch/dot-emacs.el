289cd0572c6304fbdee8dc4eceb94f80740f352e
Benoit Caccinolo <benoit.caccinolo@gmail.com>
2020-12-20 08:39:50 +0100

    update

--- emacs-from-scratch/dot-emacs.el
+++ emacs-from-scratch/dot-emacs.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode magit))))
+ '(package-selected-packages (quote (treemacs markdown-mode magit))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
